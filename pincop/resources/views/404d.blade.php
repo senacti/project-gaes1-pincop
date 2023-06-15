85c343c65ff7b8a1d52617d8bafd500397b1914c
JuanDRG436 <131384185+JuanDRG436@users.noreply.github.com>
2023-06-15 13:36:40 -0500

    p

---
+++ pincop/resources/views/404d.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es" >
+<head>
+  <meta charset="UTF-8">
+  <title>Error 404</title>
+  <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css'><link rel="stylesheet" href="../css/404.css">
+  <link rel="icon" href= {{ asset('img/tenis ° pin.png') }}>
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<link href='https://fonts.googleapis.com/css?family=Anton|Passion+One|PT+Sans+Caption' rel='stylesheet' type='text/css'>
+<body>
+
+        <!-- Error Page -->
+            <div class="error">
+                <div class="container-floud">
+                    <div class="col-xs-12 ground-color text-center">
+                        <div class="container-error-404">
+                            <div class="clip"><div class="shadow"><span class="digit thirdDigit"></span></div></div>
+                            <div class="clip"><div class="shadow"><span class="digit secondDigit"></span></div></div>
+                            <div class="clip"><div class="shadow"><span class="digit firstDigit"></span></div></div>
+                            <div class="msg">OH!<span class="triangle"></span></div>
+                        </div>
+                        <h2 class="h1">¡Lo siento! Página no encontrada</h2>
+                        <h5><center><a href="{{url('/dashboard')}}"" class="boton">Volver atras</a></center></h5>
+                    </div>
+                </div>
+            </div>
+        <!-- Error Page -->
+</body>
+<!-- partial -->
+    <script  src={{ asset('js/404.js') }}></script>
+
+</body>
+</html>
---
+++ pincop/routes/web.php
@@ -0,0 +1,70 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view ('Index');
+});
+
+Route::get('/inicio', function () {
+    return view ('inicio');
+});
+
+Route::get('/registro', function () {
+    return view ('registro');
+});
+
+Route::get('/recuperar', function () {
+    return view ('recuperar');
+});
+
+Route::get('/inventario', function () {
+    return view ('inventario');
+});
+
+Route::get('/dashboard', function () {
+    return view ('dashboard');
+});
+
+Route::get('/hombre', function () {
+    return view ('hombre');
+});
+
+Route::get('/mujer', function () {
+    return view ('mujer');
+});
+
+Route::get('/niño', function () {
+    return view ('niño');
+});
+
+Route::get('/nuevo', function () {
+    return view ('nuevo');
+});
+
+Route::get('/404', function () {
+    return view ('404');
+});
+
+Route::get('/404d', function () {
+    return view ('404d');
+});
+
+Route::get('/500', function () {
+    return view ('500');
+});
+
+Route::get('/500d', function () {
+    return view ('500d');
+});
