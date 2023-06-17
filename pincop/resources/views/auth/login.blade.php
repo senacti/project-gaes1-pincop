e0a53d97bb450c8da2da2813fef71cd8e0637156
JuanDRG436 <131384185+JuanDRG436@users.noreply.github.com>
2023-06-17 16:36:04 -0500

    Update login.blade.php

--- pincop/resources/views/auth/login.blade.php
+++ pincop/resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
 
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container">
-          <a class="navbar-brand" href="{{ URL('/') }}">Custom Login Register</a>
+          <a class="navbar-brand" href="{{ URL('/') }}">Pincop</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
